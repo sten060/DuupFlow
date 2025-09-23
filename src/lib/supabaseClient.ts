71c4e3a52f2965848361b3d9b2d453a6edd72eb2
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2025-09-23 23:20:52 +0700

    fix supabaseClient.ts avec clients serveur et browser

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,9 +2,17 @@
 import { createBrowserClient, createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
+// Client côté navigateur
+export function createClientBrowser() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
+
+// Client côté serveur
 export function createClientServer() {
   const cookieStore = cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -13,16 +21,7 @@ export function createClientServer() {
         get(name: string) {
           return cookieStore.get(name)?.value;
         },
-        set() {},
-        remove() {},
       },
     }
-  );
-}
-
-export function createClientBrowser() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
